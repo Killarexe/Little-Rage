2b19fb06a1b000c3259fb0bed01c2b8a97a0271e
Killar.exe <killar.exe@gmail.com>
2024-09-08 21:41:45 +0200

    Switched to composed system

---
+++ scripts/level/tiles/CheckpointTileComponent.gd
@@ -0,0 +1,18 @@
+extends Node
+class_name CheckpointTileComponent
+
+@export_category("Checkpoint components")
+@export var interactive_tile: InteractiveTile
+@export var tile_off_texture: Sprite2D
+@export var checkpoint_particle: PackedScene
+
+func switch_off() -> void:
+	tile_off_texture.visible = true
+
+func _on_checkpoint_tile_on_body_entered(body: Node2D) -> void:
+	if body is PlayerComponent && tile_off_texture.visible:
+		get_tree().call_group("CheckpointTiles", "switch_off")
+		tile_off_texture.visible = false
+		var spawn_point: Vector2 = interactive_tile.global_position - Vector2(0, 32)
+		body.get_node("DeathComponent").spawn_point = spawn_point
+		Game.instanceNodeAtPos(checkpoint_particle, body.get_parent(), spawn_point + Vector2(0, 16))
--- scripts/level/tiles/InteractiveTile.gd
+++ scripts/level/tiles/InteractiveTile.gd
@@ -1,15 +1,12 @@
 extends StaticBody2D
 class_name InteractiveTile
 
+signal on_body_entered(body: Node2D)
+signal on_body_exited(body: Node2D)
+
 @export_category("Interaction Tile Dependencies")
 @export var interatcion_area: Area2D
 
 func _ready() -> void:
-	interatcion_area.body_entered.connect(on_body_entered)
-	interatcion_area.body_exited.connect(on_body_exited)
-
-func on_body_entered(body: Node2D) -> void:
-	pass
-
-func on_body_exited(body: Node2D) -> void:
-	pass
+	interatcion_area.body_entered.connect(func(body: Node2D): on_body_entered.emit(body))
+	interatcion_area.body_exited.connect(func(body: Node2D): on_body_exited.emit(body))
